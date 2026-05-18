db8b339a2456b893382be2493988ab4358126c89
MellArii <art76394@gmail.com>
2026-05-18 21:59:34 +0300

    refactor: usless files deleted

--- build/CMakeFiles/main_app.dir/characters.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main_app.dir/characters.cpp.obj: \
- C:\Users\art76\OneDrive\Desktop\projects\DSTCalculator\characters.cpp \
- C:\Users\art76\OneDrive\Desktop\projects\DSTCalculator\characters.h
